e427d0b112e6502f15de56e680e29fba45710d87
BleuSkell <kyasowi1@gmail.com>
2024-08-29 19:14:50 +0200

    guest overview added, background image added

--- create.php
+++ create.php
@@ -1,35 +1,30 @@
 <?php
-
     include('conf/conf.php');
-
+    
     $dsn = "mysql:host=$dbHost;
-        dbname=$dbName;
-        charset=UTF8";
+            dbname=$dbName;
+            charset=UTF8";
 
     $pdo = new PDO($dsn, $dbUser, $dbPass);
 
-    $sql = "INSERT INTO guests (Name,
-                                ranks)
-                        VALUES  (:Name
-                                ,:ranks)";
-    
+    $sql = "SELECT *
+            FROM guests
+            ORDER BY ranks";
+
     $statement = $pdo->prepare($sql);
-    $statement->bindValue(':Name', $_POST['Name'], PDO::PARAM_STR);
-    $statement->bindValue(':ranks', $_POST['ranks'], PDO::PARAM_STR);
 
     $statement->execute();
-    
-    $dataRows = '';
 
-    $dataRows = "<tr>
-        <td>{$_POST['Name']}</td>
-        <td>{$_POST['ranks']}</td>
-    </tr>";
+    $result = $statement->fetchAll(PDO::FETCH_OBJ);
 
+    $tableRows = "";
 
-    $data = [
-        'dataRows' => $dataRows
-    ];
+    foreach ($result as $guests) {
+        $tableRows .= "<tr>
+                            <td>$guests->name</td>
+                            <td>$guests->ranks</td>
+                        <tr>";
+    };
 ?>
 
 <!DOCTYPE html>
@@ -39,23 +34,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guests</title>
     <link rel="stylesheet" href="guest.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <div id="test">
+    <div id="banner">
         <img src="foto's/BMTH.png" alt="Bring me the horizon band">
 
-        <table class="table guestList">
-            <thead>
-                <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Ranks</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?= $data['dataRows']; ?>
-            </tbody>
-        </table>
+        <div class="container">
+            <div class="row text-center justify-content-center align-items-center">
+                <div class="col-8 test">
+                    <table class="table table-borderless table-striped guestlist">
+                        <thead>
+                            <th>name</th>
+                            <th>rank</th>
+                        </thead>
+                        <tbody>
+                            <?php echo $tableRows; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
     </div>
 </body>
 </html>
--- ticket.php
+++ ticket.php
@@ -6,23 +6,43 @@
     <title>Tickets</title>
 </head>
 <body>
-    <h1>form test</h1>
+    <form action="ticket.php" method="post">
+        <label for="name">Fill your name in:</label>
+        <input type="text" name="name" id="name">
 
-    <form action="create.php" method="post">
-        <input type="text" name="Name" id="Name" placeholder="Fill your name here" required>
+        <label for="ranks">rank 1</label>
+        <input type="radio" name="ranks" id="ranks" value="rank1">
 
-        <div class="rank">
-            <label for="ranks">rank 1</label>
-            <input type="radio" name="ranks" id="ranks" value="rank1">
+        <label for="ranks">rank 2</label>
+        <input type="radio" name="ranks" id="ranks" value="rank2">
 
-            <label for="ranks">rank 2</label>
-            <input type="radio" name="ranks" id="ranks" value="rank2">
+        <label for="ranks">rank 3</label>
+        <input type="radio" name="ranks" id="ranks" value="rank3">
 
-            <label for="ranks">rank 3</label>
-            <input type="radio" name="ranks" id="ranks" value="rank3">
-        </div>
-
-        <button type="submit">Submit</button>
+        <button type="submit">submit</button>
     </form>
+
+    <a href="create.php">test</a>
 </body>
 </html>
+
+<?php
+    include('conf/conf.php');
+
+    $dsn = "mysql:host=$dbHost;
+                dbname=$dbName;
+                charset=UTF8";
+
+    $pdo = new PDO($dsn, $dbUser, $dbPass);
+
+    $sql = "INSERT INTO guests  (name,
+                                ranks)
+                        VALUES  (:name
+                                ,:ranks)";
+    
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(':name', $_POST['name'], PDO::PARAM_STR);
+    $statement->bindValue(':ranks', $_POST['ranks'], PDO::PARAM_STR);
+
+    $statement->execute();
+?>
