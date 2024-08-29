442296325dae3bd6255d2700a8fc1086e4df76c3
BleuSkell <kyasowi1@gmail.com>
2024-08-29 12:24:11 +0200

    finished database ticket functionality

--- create.php
+++ create.php
@@ -9,25 +9,15 @@
     $pdo = new PDO($dsn, $dbUser, $dbPass);
 
     $sql = "INSERT INTO guests (Name,
-                                rank)
+                                ranks)
                         VALUES  (:Name
-                                ,:rank)";
+                                ,:ranks)";
     
     $statement = $pdo->prepare($sql);
     $statement->bindValue(':Name', $_POST['Name'], PDO::PARAM_STR);
-    $statement->bindValue(':rank', $_POST['rank'], PDO::PARAM_STR);
+    $statement->bindValue(':ranks', $_POST['ranks'], PDO::PARAM_STR);
 
     $statement->execute();
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php echo $_POST[$sql]; ?>
-</body>
-</html>
+    echo implode(" ", $_POST);
+?>
--- ticket.php
+++ ticket.php
@@ -10,11 +10,17 @@
 
     <form action="create.php" method="post">
         <input type="text" name="Name" id="Name" placeholder="Fill your name here" required>
-        <select name="rank" id="rank">
-            <option value="rank1">Rank1</option>
-            <option value="rank2">Rank2</option>
-            <option value="rank3">Rank3</option>
-        </select>
+
+        <div class="ranks">
+
+            <label for="ranks">rank1</label>
+            <input type="radio" name="ranks" id="ranks" value="rank1" >
+            <label for="ranks">rank2</label>
+            <input type="radio" name="ranks" id="ranks" value="rank2" >
+            <label for="ranks">rank3</label>
+            <input type="radio" name="ranks" id="ranks" value="rank3" >
+
+        </div>
 
         <button type="submit">Submit</button>
     </form>
